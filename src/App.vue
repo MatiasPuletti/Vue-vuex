a56016be63125bea4c3215a7e1742c6bde5a0713
matiaspuletti <pulettimatias@gmail.com>
2021-09-08 08:41:42 +0200

    Implement namespaced modules

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <change-counter></change-counter>
   </base-container>
   <base-container title="Auth">
-  <user-auth></user-auth>
+    <user-auth></user-auth>
   </base-container>
 </template>
 
@@ -34,7 +34,7 @@ export default {
     addOne() {
       // this.$store.dispatch('increase', { value: 10 });
       this.$store.dispatch({
-        type: 'increase',
+        type: 'numbers/increase',
         value: 10
       });
     }
